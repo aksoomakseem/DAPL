d0aa0f12092a28287994db003409b2c27701c9f9
Dai Leshan <xuzenglin22@nudt.edu.cn>
2026-05-06 16:39:08 +0800

    Convert DAPrompt from submodule to regular folder

---
+++ DAPrompt/scripts/eval.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# custom config
+DATA=/home/dails26/X/mywork/data # 你的 office-home 数据集所在的父目录
+TRAINER=DAPL
+
+DATASET=$1
+CFG=$2  # config file
+T=$3 # temperature
+TAU=$4 # pseudo label threshold
+U=$5 # coefficient for loss_u
+NAME=$6 # job name
+
+
+for SEED in 1
+do
+    DIR=output/${DATASET}/${TRAINER}/${CFG}/${T}_${TAU}_${U}_${NAME}/seed_${SEED}
+    python train.py \
+    --root ${DATA} \
+    --seed ${SEED} \
+    --trainer ${TRAINER} \
+    --dataset-config-file configs/datasets/${DATASET}.yaml \
+    --config-file configs/trainers/${TRAINER}/${CFG}.yaml \
+    --model-dir ${DIR} \
+    --output-dir ${DIR} \
+    --eval-only
+done
